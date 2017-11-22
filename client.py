61e7167ffc4c6154b93a408b3d2aecd3941db8ed
Mitch Myers <mmyers39@gatech.edu>
2017-11-22 15:27:09 -0600

    Adding the final comments to the code

--- client.py
+++ client.py
@@ -2,23 +2,30 @@ import sys
 import socket
 import struct
 
+# Pull out the server IP and Port from the command line arguments
 server_ip = sys.argv[1]
 server_port = sys.argv[2]
 
+# Create a new client socket
 try:
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error:
     print("Error binding to server socket")
     sys.exit()
+
+# Connect to the server IP address and port
 client_sock.connect((server_ip, int(server_port)))
 
+# Ask if user is ready to start the game
 ready_ans = raw_input('Ready to start the game? (y/n): ')
 
-
+# Reformat the user's answer and if it is not a y (or Y) then close the socket
 if not ready_ans.islower():
     user_input = ready_ans.lower()
 if ready_ans != 'y':
     client_sock.close()
+# If user ready to play, then send an empty packet with msg_flag set to 0 to signal to the server to send an
+# encoded word
 else:
     fmt = 'B{}s'.format(len(' '))
     client_struct = struct.pack(fmt, 0, ' ')
@@ -27,19 +34,26 @@ else:
 guesses = []
 
 while True:
+    # Receive a message from the server
     server_msg = client_sock.recv(1024)
+    # Unpack the msg_flag to differentiate between a control packet and a non-control packet
     msg_flag = struct.unpack('B', server_msg[0])[0]
+    # If it's a control packet, then unpack the word length, num incorrect guesses, encoded word, and incorrect guesses
     if msg_flag is 0:
         word_length = struct.unpack('B', server_msg[1])[0]
         num_inc_guesses = struct.unpack('B', server_msg[2])[0]
         enc_msg = struct.unpack('{}s'.format(word_length * 2), server_msg[3: 3 + (word_length * 2)])[0]
         inc_guesses = struct.unpack('{}s'.format(num_inc_guesses * 2), server_msg[3 + (word_length * 2):])[0]
 
+        # Print the current encoded word
         print(enc_msg)
+        # Print all of the incorrect guesses by the user thus far
         print('Incorrect Guesses: ' + inc_guesses)
 
         valid_input = False
         user_input = None
+        # Check if the letter guessed by the user fits the criteria (i.e. is only one letter)
+        # If false, then prompt the user to guess again and again until they input a valid answer
         while valid_input is False:
             user_input = raw_input('Letter to guess: ')
             print('\n')
@@ -51,10 +65,14 @@ while True:
                 user_input = user_input.lower()
             if user_input in guesses:
                 print("Error! Letter " + user_input + " has been guessed before, please guess another letter.\n")
+        # Keep track of the guesses to make sure the user doesn't guess the same letter again
         guesses.append(user_input)
+        # Pack the guessed letter into a struct and send it to the client
         fmt = 'B{}s'.format(len(user_input))
         client_struct = struct.pack(fmt, len(user_input), user_input)
         client_sock.sendall(client_struct)
+    # The server has sent a game ending packet which means the user either guessed the correct answer, or the user
+    # used up all guesses and it's game over. Print out the message and close the client socket
     else:
         enc_msg = struct.unpack('{}s'.format(len(server_msg[1:])), server_msg[1:])[0]
         print(enc_msg)
--- server.py
+++ server.py
@@ -4,57 +4,64 @@ from thread import *
 import numpy as np
 import struct
 
+# Pulling out the port number from the command line argument
 port_num = sys.argv[1]
 word_dict = []
+
+# Process to populate the word dictionary list if a file with words is given as a command line arg
 try:
-	#check if argument is there
+	# Check if argument is there
 	file_name = sys.argv[2]
 
-	#perform I/O
+	# Perform I/O
 	f = open(str(file_name), "r")
 	content_list = []
 	for line in f:
 		content_list.append(line.strip('\n'))
 	content_list[:] = [i for i in content_list if i != '']
 
-	#populate word dict
+	# Populate word dict
 	for i in range(1,len(content_list)):
 		word_dict.append(content_list[i])
 except:
 	word_dict = ['panda', 'snake', 'dog', 'rhino', 'giraffe', 'donkey', 'lion', 'flamingo', 'koala', 'horse',
 	             'gorilla', 'buffalo', 'spider', 'mouse', 'eagle']
 
-
+# Helper function that takes in the current encoded word and puts the letter in the correct spot if its in the word
 def decode_word(word, enc_word, letter):
+    # Change the string into a list for ease of mutability
     dec_word_list = list(enc_word)
     for i in range(len(word)):
+        # Checks if the current letter is the guessed letter
         if word[i] is letter:
+            # If so, then change the letter's spot in the encoded word from '_' to the guessed letter
             dec_word_list[i * 2] = letter
+    # Since a list of characters is used for the decoded word, this joins all of the characters into a string
     return ''.join(dec_word_list)
 
 
-def encode_word(word):
-    enc_word_list = list(word)
-    for i in range(len(word)):
-        enc_word_list[i] = '_'
-    return ''.join(enc_word_list)
-
-
-
+# Initialize the server socket
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
+    # Bind to the socket port
     server_sock.bind(('localhost', int(port_num)))
 except socket.error:
     print("Error binding to server socket")
     sys.exit()
 
+# Begin listening for incoming connections from clients
 server_sock.listen(10)
 num_clients = 0
 
+# Helper method to create new threads to support multiple clients at the same time
 def thread(client_connect, addr):
     global num_clients
+
+    # Chooses a random word from the word dictionary list
     word = np.random.choice(word_dict, 1)[0]
 
+    # Encodes the intial word into a series of '_' characters
+    # Uses a list for mutability purposes and then converts the list into a string
     enc_word = []
     for i in range(len(word)):
         enc_word.append('_ ')
@@ -64,55 +71,73 @@ def thread(client_connect, addr):
     cor_guesses = []
     cor_count = 0
     while True:
+
+        # Receive the message from the client
         client_msg = client_connect.recv(1024)
-        #try to unpack. if exception, this means server overload. -> break
+        # Try to unpack. if exception, this means server overload. -> break
         try:
             client_msg_flag = struct.unpack('B', client_msg[0])[0]
         except:
             break
+
+        # Unpack the guessed letter from the client
         guessed_letter = struct.unpack('{}s'.format(1), client_msg[1])[0]
+        # Check if it's a control packet, if so, then send the initial control packet with the completely encoded word
         if client_msg_flag is 0:
+            # Format the control packet so that it's
+            # [flag | word_length | num_incorrect_guesses | encoded_word | inc_guesses]
             fmt = 'BBB{}s{}s'.format(len(enc_word), len(inc_guesses * 2))
             struct_con = struct.pack(fmt, 0, len(word), len(inc_guesses), enc_word, '')
             client_connect.sendall(struct_con)
         else:
+            # Decode the word based on the letter guessed
             curr_enc_word = decode_word(word, enc_word, guessed_letter)
+            # Check if the letter is not in the word and add it to the incorrect guesses if it is not already in there
             if guessed_letter not in word and (guessed_letter + ' ') not in inc_guesses:
                 inc_guesses.append(guessed_letter + ' ')
+            # Check if the guessed letter is in the word and if so then add it to the correct guesses
             elif guessed_letter in word and guessed_letter not in cor_guesses:
                 cor_guesses.append(guessed_letter)
                 cor_count += word.count(guessed_letter)
+            # If the user guessed wrong and it's the sixth incorrect guess then the user loses and it's game over
+            # Send the game over packet and break out of the loop to close the client connection
             if len(inc_guesses) > 5:
                 game_over_msg = 'Game Over!! The word was: ' + word
                 fmt = 'B{}s'.format(len(game_over_msg))
                 game_over_struct = struct.pack(fmt, len(game_over_msg), game_over_msg)
                 client_connect.sendall(game_over_struct)
                 break
+            # If the user guesses the last un-guessed letter correctly, then the user wins and the game is over
+            # Send the victory message packet and break out of the loop to close the client connection
             if cor_count is len(word):
                 victory_msg = 'You won!! The word was: ' + word
                 fmt = 'B{}s'.format(len(victory_msg))
                 victory_struct = struct.pack(fmt, len(victory_msg), victory_msg)
                 client_connect.sendall(victory_struct)
                 break
+            # Send the new decoded word in the newly updated control packet back to the client
             fmt = 'BBB{}s{}s'.format(len(curr_enc_word), len(inc_guesses * 2))
             struct_con = struct.pack(fmt, 0, len(word), len(inc_guesses), curr_enc_word, ''.join(inc_guesses))
             client_connect.sendall(struct_con)
             enc_word = curr_enc_word
+    # The client connection is closing so decrement the number of active clients
     num_clients -= 1
     print("End the connection from {} : {}".format(addr[0], str(addr[1])))
+    # End the connection to the client
     client_connect.close()
 
 while True:
+    # Accept a new client connection
     client_conn, addr = server_sock.accept()
 
-    #server is overloaded
+    # Server is overloaded
     if client_conn and addr and num_clients is 3:
         serv_overload = 'Server overloaded!'
         so_struct_fmt = 'B{}s'.format(len(serv_overload))
         so_struct = struct.pack(so_struct_fmt, len(serv_overload), serv_overload)
         client_conn.sendall(so_struct)
 
-    #allow new client to connect and play
+    # Allow new client to connect and play
     if client_conn and addr:
         num_clients += 1
         print("Get connected from {} : {}".format(addr[0], str(addr[1])))
